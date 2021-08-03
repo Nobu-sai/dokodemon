64c6c37a605340443b3c9d7bac0126fd6642e9f6
Nobuya Saito <snz0901@gmail.com>
2021-08-03 11:52:17 +0000

    Finish layout and routes

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -6,9 +6,7 @@ class ApplicationHelperTest < ActionView::TestCase
     base_title = "Ruby on Rails Tutorial Sample App"
     
     assert_equal full_title,         "#{base_title}"
-    assert_equal full_title("About"), "About" + " | " + base_title
     assert_equal full_title("Help"), "Help" + " | " + base_title
-    assert_equal full_title("Contact"), "Contact" + " | " + base_title
   end
 end
 
