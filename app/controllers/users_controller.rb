81973de2758a1234071f090601f9f1d5f48f0a46
Nobuya Saito <snz0901@gmail.com>
2021-08-07 12:28:13 +0000

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,8 @@ class UsersController < ApplicationController
       #   P
       #   : evernote:///view/180370944/s350/f853988c-9e8b-4f0a-63e3-4702861b2711/77812575-71c1-4561-9362-9c31a7a32180
     if @user.save
+      reset_session
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
