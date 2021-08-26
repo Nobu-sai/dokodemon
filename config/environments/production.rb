6ae5d5e93eb3868f61daca13cc0fe9c413c35958
snz0901@gmail.com <ngbt0409>
2021-08-26 23:54:31 +0900

    Production Mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'quiet-plains-85019.herokuapp.com'
+  host = 'desolate-sea-27090.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
