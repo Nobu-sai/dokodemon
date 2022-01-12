3c4a5ca7430021fe378c490ad32f2b5de624d7b3
snz0901@gmail.com <ngbt0409>
2022-01-12 16:49:42 +0900

    Try.16 - loop array of batches in the feed view -> paginate cannot be done as a single bunch

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,42 +4,39 @@ class StaticPagesController < ApplicationController
     
     # Try 
     # => Use an Array of Batches
-    @blog_posts_batches = blog_posts_feed 
-    # # @blog_posts_batches = blog_posts_feed.paginate(page: params[:page]) 
+    @blog_posts_batches = blog_posts_feed
+    # @blog_posts_batches = blog_posts_feed.paginate(page: params[:page]) 
 
-    # @blog_posts_feed.paginate(page: params[:page])
-    puts "Blog Records amount #{Blog.count}"
+    # puts "Blog Records amount #{Blog.count}"
     # puts "@blog_posts_batches #{@blog_posts_batches}"
-    puts "@blog_posts_batches.count #{@blog_posts_batches.count}"
-    puts "@blog_posts_batches.class #{@blog_posts_batches.class}"
-    puts "@blog_posts_batches.first #{@blog_posts_batches.first}"
-    puts "@blog_posts_batches[0] #{@blog_posts_batches[0]}"
-    puts "@blog_posts_batches[0].count #{@blog_posts_batches[0].count}"
-    @blog_posts_batches.each do | batch |  
-    # # blog_posts_batches.each do | batch | 
-    # #   batch.inspect
-      # puts "batch: #{batch}"
-      puts "batch.class: #{batch.class}"
-      puts "batch.count: #{batch.count}"
-      # puts "batch.first #{batch.first.count}"
-        # => NoMethodError (undefined method `count' for #<Blog:0x00007fee78324718>):
-      # puts "batch.second #{batch.second.count}"
-        # => NoMethodError (undefined method `count' for #<Blog:0x00007fee78324718>):
-      puts "batch[0] #{batch[0]}"
-      # @blog_posts_feed = batch.paginate(page: params[:page])
+    # puts "@blog_posts_batches.count #{@blog_posts_batches.count}"
+    # puts "@blog_posts_batches.class #{@blog_posts_batches.class}"
+    # puts "@blog_posts_batches.first #{@blog_posts_batches.first}"
+    # puts "@blog_posts_batches[0] #{@blog_posts_batches[0]}"
+    # puts "@blog_posts_batches[0].count #{@blog_posts_batches[0].count}"
+    # @blog_posts_batches.each do | batch |  
+    # # # blog_posts_batches.each do | batch | 
+    # # #   batch.inspect
+    #   # puts "batch: #{batch}"
+    #   puts "batch.class: #{batch.class}"
+    #   puts "batch.count: #{batch.count}"
+    #   # puts "batch.first #{batch.first.count}"
+    #     # => NoMethodError (undefined method `count' for #<Blog:0x00007fee78324718>):
+    #   # puts "batch.second #{batch.second.count}"
+    #     # => NoMethodError (undefined method `count' for #<Blog:0x00007fee78324718>):
+    #   puts "batch[0] #{batch[0]}"
+    #   # @blog_posts_feed = batch.paginate(page: params[:page])
 
-      @blog_posts_feed = []
+    #   @blog_posts_feed = []
       
-      batch.each do | post | 
-        @blog_posts_feed << post
-        puts "post #{post}" 
+    #   batch.each do | post | 
+    #     @blog_posts_feed << post
+    #     puts "post #{post}" 
 
-      end 
-      
-      puts "@blog_posts_feed (added posts directly) count #{@blog_posts_feed.count}"
-      @blog_posts_feed.paginate(page: params[:page])
+    #   end 
 
-    end
+
+    # end
 
     
     if logged_in?
