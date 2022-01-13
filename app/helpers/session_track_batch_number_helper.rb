7b3687f4a50783f1ce0b9c5c52ce8b439a6852f5
snz0901@gmail.com <ngbt0409>
2022-01-13 19:55:33 +0900

    Solution
    - Conditionally increase the session[:batch_number] as well as conditionally SHOWING the Previous Button
    	- Make the @total_batches accessible from the app/helpers/session_track_batch_number_helper.rb

--- app/helpers/session_track_batch_number_helper.rb
+++ app/helpers/session_track_batch_number_helper.rb
@@ -14,9 +14,9 @@ module SessionTrackBatchNumberHelper
 				# - Problem > whqen - next button to the last page -> refresh- click next feed button > what - undefined method `count' for nil:NilClass 
 					# = 523998b75fb6222533d179124ee81691a2b75ac0
 			session[:batch_number] += 1 
-		elsif direction == "previous" 
+		elsif direction == "previous" && session[:batch_number] > 0
 			session[:batch_number] -= 1 
-			
+
 		elsif clicked_page 
 			puts "clicked_page #{clicked_page}"
 			session[:batch_number] = clicked_page.to_i - 1
