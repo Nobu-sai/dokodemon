c4fbed7e9c66898c62be09abfbcde217f9d91054
snz0901@gmail.com <ngbt0409>
2022-01-14 20:44:25 +0900

    Solution
    - Blunder in using session[:total_batches] instead of session[:batch_size]

--- app/helpers/session_define_batch_size_helper.rb
+++ app/helpers/session_define_batch_size_helper.rb
@@ -1,7 +1,7 @@
 module SessionDefineBatchSizeHelper 
 	def define_batch_size(batch_size = nil)
 		if batch_size
-			session[:total_batches] = batch_size
+			session[:batch_size] = batch_size
 		else
 			session[:batch_size] = 1000
 		end	
