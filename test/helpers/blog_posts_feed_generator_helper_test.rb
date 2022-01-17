69b50d284cfc240601cc760119ea34300d67cb30
snz0901@gmail.com <ngbt0409>
2022-01-17 19:20:23 +0900

    Case - Variable Parameters & Keyword Parameters - In app/helpers/fetch_blog_posts_helper.rb:
    Context
    
    Definition
    def fetch_blog_posts_as_a_batch(*direction, *clicked_page, batch_size:)
    	Or
    def fetch_blog_posts_as_a_batch(*direction, *clicked_page, batch_size:);
    Output
    ```
    ...                              ^
    /Users/qitengzhanye/todo-programming/learning/output/dokodemon/app/helpers/fetch_blog_posts_helper.rb:22: syntax error, unexpected tLABEL
    ...ion, *clicked_page, batch_size:);
    ...                    ^~~~~~~~~~~
    /Users/qitengzhanye/todo-programming/learning/output/dokodemon/app/helpers/fetch_blog_posts_helper.rb:58: syntax error, unexpected end, expecting end-of-input
    ```

--- test/helpers/blog_posts_feed_generator_helper_test.rb
+++ test/helpers/blog_posts_feed_generator_helper_test.rb
@@ -24,7 +24,8 @@ class BlogPostsFeedGeneratorHelperTest < ActionView::TestCase
       total_batches = calculate_total_batches(100)
       
       for i in 0..total_batches -1 do        
-        @blog_posts_batch = fetch_blog_posts_as_a_batch("next", nil, 100)
+        @blog_posts_batch = fetch_blog_posts_as_a_batch("next", 100)
+        # @blog_posts_batch = fetch_blog_posts_as_a_batch("next", batch_size: 100)
         @blog_posts_batch.each do | post |
           all_blog_posts << post           
         end
@@ -43,7 +44,7 @@ class BlogPostsFeedGeneratorHelperTest < ActionView::TestCase
     log_in_as(current_user)
     assert is_logged_in?     
 
-	  @blog_posts_batch = fetch_blog_posts_as_a_batch(nil, nil, 100)
+	  @blog_posts_batch = fetch_blog_posts_as_a_batch(batch_size: 100)
 
     # Blog Posts by the users the Current User is FOLLOWING => Should BE included. 
       @blog_posts_batch.include?(@lana)
