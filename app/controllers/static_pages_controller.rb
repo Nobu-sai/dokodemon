3aa8832fde91ae0b6d21bfbd960f38cd242b6a6b
snz0901@gmail.com <ngbt0409>
2022-01-20 16:13:58 +0900

    Problem - NameError in StaticPagesController#home - uninitialized constant StaticPagesController::FetchBlogPostsFeedQuery

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,7 +7,7 @@ class StaticPagesController < ApplicationController
     batch_size = 100
     # @total_batches = calculate_total_batches(batch_size)
     # @blog_posts_feed = fetch_blog_posts_as_a_batch(params[:direction], params[:clicked_page], batch_size: batch_size) 
-    @blog_posts_feed = FetchBlogPostsFeedQuery.new.fetch_blog_posts_as_a_batch
+    @blog_posts_feed = FetchBlogPostsFeedQuery.new.fetch_blog_posts_as_a_batch(params[:direction], params[:clicked_page], batch_size: batch_size)
     
     if logged_in?
       @blog  = current_user.blogs.build      
