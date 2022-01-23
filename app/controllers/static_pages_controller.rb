468553178be214264a52ca02ed88530305ed007f
snz0901@gmail.com <ngbt0409>
2022-01-23 21:04:44 +0900

    Try to track Batch Number in View => Problem - ActionView::Template::Error (undefined method `>' for nil:NilClass):

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,7 +6,7 @@ class StaticPagesController < ApplicationController
     
     batch_size = 100
    
-    @blog_posts_feed = BlogPostsFeedQuery.new.fetch_blog_posts_as_a_batch(current_user: current_user, batch_size: batch_size, batch_number: track_batch_number(params[:direction], params[:clicked_page], batch_size))
+    @blog_posts_feed = BlogPostsFeedQuery.new.fetch_blog_posts_as_a_batch(current_user: current_user, batch_size: batch_size, batch_number: track_batch_number(params[:batch_number]))
     
     if logged_in?
       @blog  = current_user.blogs.build      
---
+++ app/helpers/calculate_batch_number_helper.rb
@@ -0,0 +1,24 @@
+
+module CalculateBatchNumberHelper
+	include CalculateTotalBatchesService
+
+	
+	def calculate_batch_number(clicked_button: )
+
+		total_batches = calculate_total_batches(batch_size)
+	
+		if clicked_button == "next" && session[:batch_number] < (total_batches) -1
+			# session[:batch_number] < (calculate_total_batches) -1
+				# Pro
+				# - Problem > whqen - next button to the last page -> refresh- click next feed button > what - undefined method `count' for nil:NilClass 
+					# = 523998b75fb6222533d179124ee81691a2b75ac0
+			session[:batch_number] += 1 
+		elsif clicked_button == "previous" && session[:batch_number] > 0
+			session[:batch_number] -= 1 
+
+		else			
+			session[:batch_number] = clicked.to_i - 1
+		end					
+	end
+
+end
