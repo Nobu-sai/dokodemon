cc0540c659d626750ea414155d598a22e56f37cd
snz0901@gmail.com <ngbt0409>
2022-01-24 18:21:38 +0900

    Chore - Clean

--- app/services/session_track_batch_number_service.rb
+++ app/services/session_track_batch_number_service.rb
@@ -3,12 +3,8 @@ module SessionTrackBatchNumberService
 
     def track_batch_number(batch_number) 
 
-	p
 	
 	if !session[:batch_number]
-		puts "session #{session}"
-		puts "session.class #{session.class}"
-		puts "session[:batch_number] #{session[:batch_number]}"
 		session[:batch_number] = 0 
 	else
 		# Conditional for next or previous 
