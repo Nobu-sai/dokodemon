5c00708d676e386c4cd8a4e57076014215a4e5f1
snz0901@gmail.com <ngbt0409>
2022-01-24 18:19:51 +0900

    Case - Add to_i in the app/helpers/calculate_batch_number_helper.rb/track_batch_number/Else Case
    Input
    - session[:batch_number] = batch_number.to_i
    Output
    - Error solved
    - URL Param is wierd

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,7 +5,7 @@ class StaticPagesController < ApplicationController
   def home    
     
     @batch_size = 100
-   
+    # batch_number = track_batch_number(params[:batch_number])
     @blog_posts_feed = BlogPostsFeedQuery.new.fetch_blog_posts_as_a_batch(current_user: current_user, batch_size: @batch_size, batch_number: track_batch_number(params[:batch_number]))
     
     if logged_in?
--- app/services/session_track_batch_number_service.rb
+++ app/services/session_track_batch_number_service.rb
@@ -2,13 +2,18 @@
 module SessionTrackBatchNumberService
 
     def track_batch_number(batch_number) 
+
+	p
 	
 	if !session[:batch_number]
+		puts "session #{session}"
+		puts "session.class #{session.class}"
+		puts "session[:batch_number] #{session[:batch_number]}"
 		session[:batch_number] = 0 
 	else
 		# Conditional for next or previous 
 			# - In the View 
-		session[:batch_number] = batch_number
+		session[:batch_number] = batch_number.to_i
 	end
 	
 	end
