0604095e67272bea5ae6457c1cadb9bb1ddeb96a
snz0901@gmail.com <ngbt0409>
2022-01-25 18:34:48 +0900

    Set Condition in track_batch_number() expecting session[:batch_number] is larger than 0

--- app/services/session_track_batch_number_service.rb
+++ app/services/session_track_batch_number_service.rb
@@ -15,10 +15,14 @@ module SessionTrackBatchNumberService
 	puts "batch_number #{batch_number.class}"
 	
 	if !session[:batch_number] && batch_number == nil
+		# Conditional for
+		# - The FIRST time visit to the URL
+			# => The user has no chance to click the Feed Button
+			# =>> params[:page] is Nil
 		return session[:batch_number] = 0  
-	else
-		# Conditional for next or previous 
-			# - In the View 
+	elsif session[:batch_number] > 0  
+		# Conditional for
+		# - When the user clicked the Feed Button 		
 		return session[:batch_number] = (batch_number.to_i - 1)
 	end
 	
