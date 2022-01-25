2dc1a04601a0e3cefbded4f4f5b431772a3bb3a9
snz0901@gmail.com <ngbt0409>
2022-01-25 17:09:23 +0900

    Chore - clean

--- app/services/calculate_total_batches_service.rb
+++ app/services/calculate_total_batches_service.rb
@@ -2,7 +2,6 @@
 module CalculateTotalBatchesService
 	
     def calculate_total_batches(batch_size)
-	# batch_size = session[:batch_size]
 	@total_batches = Blog.count / batch_size
 	(Blog.count - (Blog.count / batch_size * batch_size)) > 0 ? @total_batches += 1 : return 
 	
--- app/services/session_track_batch_number_service.rb
+++ app/services/session_track_batch_number_service.rb
@@ -6,17 +6,11 @@ module SessionTrackBatchNumberService
 
 	
 	if !session[:batch_number] && batch_number == nil
-		puts "SessionTrackBatchNumberService/track_batch_number/!session[:batch_number] Case"
-		puts "session[:batch_number] #{session[:batch_number]}"
 		return session[:batch_number] = 0  
-		puts "session[:batch_number] #{session[:batch_number]}"
 	else
 		# Conditional for next or previous 
 			# - In the View 
-		puts "SessionTrackBatchNumberService/track_batch_number/Else Case"
-		puts "session[:batch_number] #{session[:batch_number]}"
 		return session[:batch_number] = (batch_number.to_i - 1)
-		puts "session[:batch_number] #{session[:batch_number]}"
 	end
 	
 	end
