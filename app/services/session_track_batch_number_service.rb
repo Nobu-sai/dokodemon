5846bf401ccda85d59d581c4e0785083860d86de
snz0901@gmail.com <ngbt0409>
2022-01-25 17:31:09 +0900

    Doc

--- app/services/session_track_batch_number_service.rb
+++ app/services/session_track_batch_number_service.rb
@@ -1,8 +1,14 @@
 
+# Instruction
+## batch_number Param
+# - The Feed Page Number (Actual Batch Nmuber + 1)
+	# P
+	# - Looks better (consistent to the Page Number) in URL Param to Pass 
+
 module SessionTrackBatchNumberService
 
     def track_batch_number(batch_number) 
-
+	puts "batch_number #{batch_number}"
 	
 	if !session[:batch_number] && batch_number == nil
 		return session[:batch_number] = 0  
