edc93115379c7724c8ae748d7e828c62c3a70867
snz0901@gmail.com <ngbt0409>
2022-01-25 17:09:35 +0900

    Doc -  app/helpers/calculate_batch_number_helper.rb

--- app/helpers/calculate_batch_number_helper.rb
+++ app/helpers/calculate_batch_number_helper.rb
@@ -1,4 +1,22 @@
 
+### Instruction
+## What this does
+# - Return the Batch Number 
+	# - The Batch needs to be shown in the NEXT Controller Response 
+## When it is Called
+# - In View, the user clicked the Feed Button (for the NEXT page to see)
+
+## How to Develop
+# - Do NOT manipulat the Session directly
+	# => batch_number = session[:batch_number]
+	# P
+	# - Session is UPDATED, the purpose is ONLY CALCULATION of the Batch Number 
+# - Do NOT UPDATE the Session here Batch Num
+	# * Update of Session is done in app/services/session_track_batch_number_service.rb 
+	# P
+	# - If the session is updated here, the MEANT Batch Number is NOT returned -> ...
+
+
 module CalculateBatchNumberHelper
 	include CalculateTotalBatchesService
 
@@ -8,28 +26,14 @@ module CalculateBatchNumberHelper
 		total_batches = calculate_total_batches(batch_size)
 		
 		batch_number = session[:batch_number]
-		
 
 		if clicked_button == "next" && session[:batch_number] < (total_batches) -1
-			# session[:batch_number] < (calculate_total_batches) -1
-				# Pro
-				# - Problem > whqen - next button to the last page -> refresh- click next feed button > what - undefined method `count' for nil:NilClass 
-					# = 523998b75fb6222533d179124ee81691a2b75ac0
-			# session[:batch_number] += 1			 						 
-			# batch_number = session[:batch_number] += 1			 
-			# batch_number = (session[:batch_number] += 1).to_i			 
 			batch_number += 1
 		elsif clicked_button == "previous" && session[:batch_number] > 0
-			# session[:batch_number] -= 1 
-			# session[:batch_number] -= 1 
-			# batch_number = session[:batch_number] -= 1 
 			batch_number -= 1
 
 
 		elsif clicked_button.is_a?(Integer)			
-			# session[:batch_number] = clicked_button - 1
-			# session[:batch_number] = clicked_button - 1
-			# batch_number = session[:batch_number] = clicked_button - 1
 			batch_number = clicked_button - 1
 					
 		end	
