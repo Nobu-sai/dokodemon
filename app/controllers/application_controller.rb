2c0a964651a575246fcb289e8e70d6a7e1f09cf5
snz0901@gmail.com <ngbt0409>
2022-01-26 16:34:45 +0900

    Divide the ApplicationController/logged_in_user Method into a Policy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,19 +3,13 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
   include SessionStoreCurrentPathHelper
+  include LoggedInUserPolicy
   
   before_action :track_page_path
   
   private  
 
     # Confirms a logged-in user.
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end 
     def track_page_path
       session_store_current_path controller_name, action_name      
     end
---
+++ app/policies/logged_in_user_policy.rb
@@ -0,0 +1,10 @@
+
+module LoggedInUserPolicy
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end 
+end
