1121c19dd119b1efe1b20bba759ce99b825de57e
snz0901@gmail.com <ngbt0409>
2022-01-26 19:09:20 +0900

    Doc

--- app/policies/user_should_logged_in_policy.rb
+++ app/policies/user_should_logged_in_policy.rb
@@ -5,7 +5,29 @@ module UserShouldLoggedInPolicy
       unless logged_in?
         store_location
         # For
-        # - Redirecting the user 
+          # - 
+          # Keep CALL HERE
+          # P
+          # - One Call there 
+          # - In a bunch of Code (Ex: A File) ONE Call for ONE purpose (whose NECESSITY was DEFINED there)
+          #   => The CALLED Method Calls ANOTHER INDEPENDENT Method 
+          #   P
+          #   - Clean Code
+          #     P
+          #     - In a bunch of Code (Ex: A File) ONE Call for ONE purpose (whose NECESSITY was DEFINED there)
+          #   - Composition
+          #     P
+          #     - "The CALLED Method Calls ANOTHER INDEPENDENT Method"
+          # C
+          # - Better with SRP in one Method
+          #   C
+          #    - It IS
+            #   - For Login logic 
+            #   - The Logic for storing previuos URL is ANOTHER component 
+          #   P
+          #   - Understand 
+          #   - Maintenance
+          #   - Test 
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
