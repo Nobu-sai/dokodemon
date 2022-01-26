7ccbab5503abd5b1d78dd7eda516333c3c5d7aed
snz0901@gmail.com <ngbt0409>
2022-01-26 20:10:31 +0900

    Doc

--- app/policies/user_should_log_in_policy.rb
+++ app/policies/user_should_log_in_policy.rb
@@ -5,29 +5,12 @@ module UserShouldLogInPolicy
       unless logged_in?
         store_location
         # For
-          # - 
+          # - The Page is Redirected to login_url -> User LOGIN -> Link BACK to the PREVIOUS URL 
           # Keep CALL HERE
-          # P
-          # - One Call there 
+          # P        
           # - In a bunch of Code (Ex: A File) ONE Call for ONE purpose (whose NECESSITY was DEFINED there)
           #   => The CALLED Method Calls ANOTHER INDEPENDENT Method 
-          #   P
-          #   - Clean Code
-          #     P
-          #     - In a bunch of Code (Ex: A File) ONE Call for ONE purpose (whose NECESSITY was DEFINED there)
-          #   - Composition
-          #     P
-          #     - "The CALLED Method Calls ANOTHER INDEPENDENT Method"
-          # C
-          # - Better with SRP in one Method
-          #   C
-          #    - It IS
-            #   - For Login logic 
-            #   - The Logic for storing previuos URL is ANOTHER component 
-          #   P
-          #   - Understand 
-          #   - Maintenance
-          #   - Test 
+          #   = evernote:///view/180370944/s350/ef1b9b61-62cf-359a-1d2f-7655535eb443/77812575-71c1-4561-9362-9c31a7a32180          
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
