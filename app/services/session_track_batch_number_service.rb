1e6bfd629169ad0d6877c5bea62a95df9d6b5ceb
snz0901@gmail.com <ngbt0409>
2022-01-27 17:46:50 +0900

    Solution - hange Conditionals in  app/services/session_track_batch_number_service.rb

--- app/services/session_track_batch_number_service.rb
+++ app/services/session_track_batch_number_service.rb
@@ -8,25 +8,22 @@ module SessionTrackBatchNumberService
 			# P
 			# - Looks better (consistent to the Page Number) in URL Param to Pass 
 		# - Looks better (consistent to the Page Number) in URL Param to Pass 
-	puts "batch_number #{batch_number}"
-	puts "batch_number.class #{batch_number.class}"
-	if !session[:batch_number] && batch_number == nil
-		# Conditional for
-		# - The FIRST time visit to the URL
-			# => The user has no chance to click the Feed Button
-			# =>> params[:page] is Nil
+	if batch_number == nil || batch_number == 0 
+		# batch_number == nil
+		# For
+			# - The FIRST time visit to the URL
+				# => The user has no chance to click the Feed Button
+				# =>> params[:page] is Nil
+		# batch_number == 0 
+			# - When this is Called MULTIPLE times with EMPTY param[:page] 
+				# => In the FIRST Call, it Execute the TRUTHY Case and sets session[:batch_number] as 0 
+				# => Thus, in the Second Call, it Executes FALTHY Case and sets session[:batch_number] as -1 	
+				# Case
+					# - test/queries/blog_posts_feed_query_test.rb		
 		return session[:batch_number] = 0  
-	elsif session[:batch_number] > 0  
-		puts "In elsif session[:batch_number] > 0  "
-		puts "batch_number #{batch_number}"
-		puts "batch_number.class #{batch_number.class}"
+	else
 		# Conditional for
 		# - When the user clicked the Feed Button 		
-		# - When this is Called MULTIPLE times with EMPTY param[:page] 
-			# => In the FIRST Call, it Execute the TRUTHY Case and sets session[:batch_number] as 0 
-			# => Thus, in the Second Call, it Executes FALSY Case and sets session[:batch_number] as -1 	
-			# Case
-				# - test/queries/blog_posts_feed_query_test.rb		
 		return session[:batch_number] = (batch_number.to_i - 1)
 	end
 	
