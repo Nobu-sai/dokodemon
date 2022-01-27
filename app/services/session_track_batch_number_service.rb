98500b27afc54b15fd164d1722d3db14fe62a4c0
snz0901@gmail.com <ngbt0409>
2022-01-27 20:34:52 +0900

    Investigate

--- app/services/session_track_batch_number_service.rb
+++ app/services/session_track_batch_number_service.rb
@@ -8,8 +8,20 @@ module SessionTrackBatchNumberService
 			# P
 			# - Looks better (consistent to the Page Number) in URL Param to Pass 
 		# - Looks better (consistent to the Page Number) in URL Param to Pass 
-	if batch_number == nil || batch_number == 0 
-		# batch_number == nil
+	
+	# Values here
+		# session[:batch_number] 
+			# session[:batch_number] #=> -1
+			# session[:batch_number] == -1 #=> true
+			# !session[:batch_number] #=> false
+			# !session[:batch_number] == true #=> false
+		# batch_number 
+			# batch_number == nil #=> true
+	
+	if session[:batch_number] == -1 || batch_number == 0	
+		# puts "## track_batch_number/!session[:batch_number] || batch_number == 0"
+		puts "## track_batch_number/session[:batch_number] == nil || batch_number == 0"
+		# !session[:batch_number]
 		# For
 			# - The FIRST time visit to the URL
 				# => The user has no chance to click the Feed Button
@@ -21,11 +33,19 @@ module SessionTrackBatchNumberService
 				# Case
 					# - test/queries/blog_posts_feed_query_test.rb		
 		return session[:batch_number] = 0  
+	# elsif session[:batch_number] > 0 
+	# elsif session[:batch_number].is_a?(Integer) && session[:batch_number] > 0 
+	# elsif !session[:batch_number] == nil || session[:batch_number] > 0 
 	else
+		puts "## track_batch_number/elsif session[:batch_number] > 0"
 		# Conditional for
 		# - When the user clicked the Feed Button 		
 		return session[:batch_number] = (batch_number.to_i - 1)
+	# else
+	# 	return
 	end
+
+
 	
 	end
 end
